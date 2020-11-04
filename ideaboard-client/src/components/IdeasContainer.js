7e237fb58209a22c097bb7bec48ecdbaaf0c40ab
Uduak Essien <acushla4real@gmail.com>
2020-11-04 09:14:29 +0100

    add new idea button

--- ideaboard-client/src/components/IdeasContainer.js
+++ ideaboard-client/src/components/IdeasContainer.js
@@ -22,6 +22,9 @@ class IdeasContainer extends Component {
       render() {
         return (
           <div>
+            <button className="newIdeaButton">
+                New Idea
+            </button>
             {this.state.ideas.map((idea) => {
             return (<Idea idea={idea} key={idea.id} />)
             })}
